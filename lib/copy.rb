aa797545b361c2cab456d4cf523869322a4d36e4
Cindy Ward <cindyward@yahoo.com>
2014-08-17 00:02:15 -0700

    retrieve a copy entry by book id if not checked out or by checkout id

--- lib/copy.rb
+++ lib/copy.rb
@@ -20,6 +20,93 @@ class Copy
     copies
   end
 
+  def self.get_by_id(id)
+    results = DB.exec("SELECT * from copy WHERE id = #{id};")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id, :book_id=>@book_id})
+    end
+    copies
+  end
+
+  def self.get_by_checkout_id(checkout_id)
+    results = DB.exec("SELECT * from copy WHERE checkout_id = #{checkout_id};")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id, :book_id=>@book_id})
+    end
+    copies
+  end
+
+  def self.get_by_book_id(book_id) # find a book that's not checked out
+    results = DB.exec("SELECT * from copy WHERE book_id = #{book_id} and checkout_id = 0;")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id,
+                                          :book_id=>@book_id})
+    end
+    copies
+  end
+
+	def self.get_by_id(id)
+    results = DB.exec("SELECT * from copy WHERE id = #{id};")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id, :book_id=>@book_id})
+    end
+    copies
+  end
+
+  def self.get_by_checkout_id(checkout_id)
+    results = DB.exec("SELECT * from copy WHERE checkout_id = #{checkout_id};")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id, :book_id=>@book_id})
+    end
+    copies
+  end
+
+  def self.get_by_book_id_not_checked_out(book_id)
+    results = DB.exec("SELECT * from copy WHERE book_id = #{book_id} and checkout_id = 0;")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id,
+                                          :book_id=>@book_id})
+    end
+    copies
+  end
+
+	def self.get_by_checkout_id(checkout_id)
+    results = DB.exec("SELECT * from copy WHERE checkout_id = #{checkout_id};")
+    copies = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @checkout_id = results['checkout_id'].to_i
+      @book_id = results['book_id'].to_i
+      copies << Copy.new({:id=>@id, :checkout_id=>@checkout_id,
+                                          :book_id=>@book_id})
+    end
+    copies
+  end
+
   def ==(another_copy)
     self.checkout_id == another_copy.checkout_id && self.book_id == another_copy.book_id
   end
--- spec/copy_spec.rb
+++ spec/copy_spec.rb
@@ -20,4 +20,21 @@ describe Copy do
     expect(Copy.all).to eq [test_copy]
   end
 
+	it "is retrieved from the database by book id if it not checked out" do
+    test_book = Book.new({:title=>"The Iceman Cometh", :isbn_10=>"0300117434", :id=>1})
+    test_book.save
+    test_copy = Copy.new({:id=>1, :book_id=>test_book.id, :checkout_id=>0})
+    test_copy.save
+    test_copy1 = Copy.get_by_book_id_not_checked_out(test_book.id).first
+    expect(test_copy).to eq test_copy1
+  end
+
+	it "is retrieved from the database by checkout_id" do
+    test_book = Book.new({:title=>"The Iceman Cometh", :isbn_10=>"0300117434", :id=>1})
+    test_book.save
+    test_copy = Copy.new({:id=>1, :book_id=>test_book.id, :checkout_id=>5})
+    test_copy.save
+    test_copy1 = Copy.get_by_checkout_id(5).first
+    expect(test_copy).to eq test_copy1
+  end
 end
