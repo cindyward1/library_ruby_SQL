aaaf386649dec2de41fa5cc744dee106cc9661f2
Cindy Ward <cindyward@yahoo.com>
2014-08-17 02:25:56 -0700

    retrieve all checkout entries that are overdue

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -55,6 +55,21 @@ class Checkout
     checkouts
   end
 
+  def self.get_overdue(today_date)
+  	results = DB.exec("SELECT id, patron_id, copy_id, TO_CHAR(checkout_date, 'MM/DD/YYYY') AS checkout_date_char, " +
+    									"TO_CHAR(due_date, 'MM/DD/YYYY') AS due_date_char from checkout WHERE due_date < TO_DATE('#{today_date}', 'MM/DD/YYYY');")
+		checkouts = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @patron_id = results['patron_id'].to_i
+      @copy_id = results['copy_id'].to_i
+      @checkout_date = results['checkout_date_char']
+      @due_date = results['due_date_char']
+      checkouts << Checkout.new({:id=>@id, :patron_id=>@patron_id, :copy_id=>@copy_id, :checkout_date=>@checkout_date, :due_date=>@due_date})
+    end
+    checkouts
+  end
+
 	def ==(another_checkout)
     self.copy_id == another_checkout.copy_id && self.patron_id == another_checkout.patron_id
   end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -74,4 +74,20 @@ describe Checkout do
     expect(Checkout.all).to eq []
   end
 
+  it "retrieves all checkouts that are overdue" do
+    test_patron = Patron.new({:name=>"Cindy Ward", :id=>1, :phone_number=>"503-555-1212"})
+    test_patron.save
+    test_book = Book.new({:title=>"The Iceman Cometh", :isbn_10=>"0300117434", :id=>1})
+    test_book.save
+    test_copy = Copy.new({:book_id=>test_book.id, :checkout_id=>0})
+    test_copy.save
+    test_checkout = Checkout.new({:id=>1, :patron_id=>test_patron.id, :copy_id=>test_copy.id,
+                                  :checkout_date=>"08/16/2014", :due_date=>"09/15/2014"})
+    test_checkout.save
+    test_checkout1 = Checkout.get_overdue("09/01/2014")
+    expect(test_checkout1).to eq []
+    test_checkout2 = Checkout.get_overdue("09/30/2014")
+    expect(test_checkout2.first).to eq test_checkout
+  end
+
 end
