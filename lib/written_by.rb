38cda51ab873e684973893fec930ca56803da8d8
Cindy Ward <cindyward@yahoo.com>
2014-08-16 23:40:25 -0700

    save a written_by entry in the library database

--- lib/written_by.rb
+++ lib/written_by.rb
@@ -8,4 +8,27 @@ class Written_by
     @book_id = attributes[:book_id]
   end
 
+def self.all
+    results = DB.exec("SELECT * from written_by;")
+    written_by_array = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @author_id = results['author_id'].to_i
+      @book_id = results['book_id'].to_i
+      written_by_array << Written_by.new({:id=>@id, :author_id=>@author_id,
+                                          :book_id=>@book_id})
+    end
+    written_by_array
+  end
+
+  def ==(another_written_by)
+    self.author_id == another_written_by.author_id && self.book_id == another_written_by.book_id
+  end
+
+  def save
+    written_by = DB.exec("INSERT INTO written_by (author_id, book_id) VALUES (#{self.author_id}, " +
+                                     "#{self.book_id}) RETURNING id;")
+    @id = written_by.first['id'].to_i
+  end
+
 end
--- spec/written_by_spec.rb
+++ spec/written_by_spec.rb
@@ -12,4 +12,14 @@ describe Written_by do
     expect(test_written_by.book_id).to eq test_book.id
   end
 
+  it "is saved to the database" do
+    test_book = Book.new({:title=>"The Iceman Cometh", :isbn_10=>"0300117434", :id=>1})
+    test_book.save
+    test_author = Author.new({:name=>"Eugene ONeill", :id=>1})
+    test_author.save
+    test_written_by = Written_by.new({:author_id=>test_author.id, :book_id=>test_book.id})
+    test_written_by.save
+    expect(Written_by.all).to eq [test_written_by]
+  end
+
 end
