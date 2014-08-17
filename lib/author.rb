e0c5387bc31933d4fe9bf5e17e006c1ac40c5d8c
Cindy Ward <cindyward@yahoo.com>
2014-08-16 22:50:44 -0700

    save an author to the library database

--- lib/author.rb
+++ lib/author.rb
@@ -7,4 +7,25 @@ class Author
     @name = attributes[:name]
   end
 
+  def self.all
+    results = DB.exec("SELECT * FROM author ORDER BY name;")
+    authors = []
+    results.each do |results|
+      @id = results['id'].to_i
+      @name = results['name']
+      authors << Author.new({:id=>@id, :name=>@name})
+    end
+    authors
+  end
+
+  def ==(another_author)
+    self.name == another_author.name
+  end
+
+  def save
+    author = DB.exec("INSERT INTO author (name) VALUES ('#{self.name}') RETURNING id;")
+    @id = author.first['id'].to_i
+  end
+  
+
 end
